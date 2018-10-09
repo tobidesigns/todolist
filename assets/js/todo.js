e45c996b3506578f193ca9b467dcce11878858a5
Tobi <owner@Owners-MacBook-Air.local>
2018-10-09 18:43:07 +0100

    adding folders

---
+++ assets/js/todo.js
@@ -0,0 +1,11 @@
+//check off specific todos by clicking
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
+//Click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});

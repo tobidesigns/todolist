d40192b051fb6beeb009db55eb4c88981875ca5b
Tobi <owner@Owners-MacBook-Air.local>
2018-10-09 19:05:03 +0100

    update

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,11 +1,21 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click" , "li", function(){
 	$(this).toggleClass("completed");
 });
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input").keypress(function(event){
+if(event.which === 13){
+	//grabbing new todo
+	var todoText = $(this).val();
+	$(this).val("");
+	//create a new li and add to ul
+	$("ul").append("<li><span>x</span> "+ todoText + "</li>")
+}
 });
